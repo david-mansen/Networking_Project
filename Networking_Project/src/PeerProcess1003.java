f8614111f30f7976103d38e7a4ceba4287df3a3e
nero777x <nero777x@gmail.com>
2014-03-16 17:20:41 -0400

    Simplified PeerProcessWXYZ code

--- Networking_Project/src/PeerProcess1003.java
+++ Networking_Project/src/PeerProcess1003.java
@@ -2,16 +2,8 @@
 public class PeerProcess1003 {
 	public static void main(String[] arguments)
 	{
-		if(arguments.length!=1)
-		{
-			System.out.println("Must contain (1) argument");
-			System.exit(0);
-		}
-		else
-		{
-			int peer_id = 1003;
-			Peer thisPeer = new Peer(peer_id);
-			System.exit(0);
-		}
+		int peer_id = 1003;
+		Peer thisPeer = new Peer(peer_id);
+		System.exit(0);
 	}
 }
