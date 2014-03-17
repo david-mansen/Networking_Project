af5b5a36d7106402cba3d40a166c3e34695c1409
David Mansen <davidmansen@gmail.com>
2014-03-17 18:56:11 -0400

    Adding peerProcess.java

---
+++ Networking_Project/src/peerProcess.java
@@ -0,0 +1,17 @@
+
+public class peerProcess {
+	public static void main(String[] arguments)
+	{
+		if(arguments.length!=1)
+		{
+			System.out.println("Must contain (1) argument");
+			System.exit(0);
+		}
+		else
+		{
+			int peer_id = Integer.parseInt(arguments[0]);
+			Peer thisPeer = new Peer(peer_id);
+			System.exit(0);
+		}
+	}
+}
